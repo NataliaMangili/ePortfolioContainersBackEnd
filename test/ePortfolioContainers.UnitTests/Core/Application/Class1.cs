ae586c6165f46018fb06f7075f8fad24b9c86fab
Natalia.Mangili <nataliaperico89@gmail.com>
2024-10-19 16:47:45 -0300

    feat: Adicionando Projeto de Testes

---
+++ test/ePortfolioContainers.UnitTests/Core/Application/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ePortfolioContainers.UnitTests.Core.Application
+{
+    internal class Class1
+    {
+    }
+}

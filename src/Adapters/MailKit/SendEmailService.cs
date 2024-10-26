d257f35e61056b09b21fb6f743ddfae593f5b6a1
Natalia.Mangili <nataliaperico89@gmail.com>
2024-10-26 16:22:53 -0300

    wip: Adapter de Envio de Email

---
+++ src/Adapters/MailKit/SendEmailService.cs
@@ -0,0 +1,32 @@
+﻿using Identity.API.Ports;
+using MimeKit;
+namespace MailKit;
+
+public class SendEmailService : INotification
+{
+    private readonly SmtpSettings _smtpSettings;
+
+    public SendEmailService(SmtpSettings smtpSettings) => _smtpSettings = smtpSettings;
+
+    public async Task<bool> SendAsync(string to, string subject, string body)
+    {
+        var message = new MimeMessage();
+        message.From.Add(new MailboxAddress(_smtpSettings.SenderName, _smtpSettings.SenderEmail));
+        message.To.Add(new MailboxAddress("", to));
+        message.Subject = subject;
+
+        message.Body = new TextPart("plain")
+        {
+            Text = body
+        };
+
+        return true;
+        //using (var client = new SmtpClient())
+        //{
+        //    await client.ConnectAsync(_smtpSettings.Server, _smtpSettings.Port, MailKit.Security.SecureSocketOptions.StartTls);
+        //    await client.AuthenticateAsync(_smtpSettings.Username, _smtpSettings.Password);
+        //    await client.SendAsync(message);
+        //    await client.DisconnectAsync(true);
+        //}
+    }
+}
---
+++ src/Core/Identity.Domain/Ports/INotification.cs
@@ -0,0 +1,7 @@
+﻿namespace Identity.API.Ports;
+
+public interface INotification
+{
+    //redirect = email
+    Task<bool> SendAsync(string redirect, string subject, string body);
+}

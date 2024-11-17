45254082b4fee95bd1af8c2939a715ee656c0fd0
Natalia.Mangili <nataliaperico89@gmail.com>
2024-11-17 09:36:38 -0300

    feat: Adicionando Generators das Entitys

---
+++ test/ePortfolioContainers.UnitTests/Core/Helpers/Generator/TagGen.cs
@@ -0,0 +1,13 @@
+﻿using ePortfolio.Domain.Models.ProjectAggregate;
+
+namespace ePortfolioContainers.UnitTests.Core.Helpers.Generator;
+
+public sealed class TagGen : Faker<Tag>
+{
+    public TagGen()
+    {
+        RuleFor(x => x.Id, f => Guid.NewGuid());
+        RuleFor(x => x.Name, f => f.Lorem.Word());
+        RuleFor(x => x.UserInclusion, f => Guid.NewGuid());
+    }
+}

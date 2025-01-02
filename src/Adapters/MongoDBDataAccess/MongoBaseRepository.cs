824a877ae73fd55b61dd6fe74e1a1acfb9752e4e
Natalia.Mangili <nataliaperico89@gmail.com>
2025-01-02 16:28:31 -0300

    feat: Ajustes e Correções Gerais para o MongoDB

--- src/Adapters/MongoDBDataAccess/MongoBaseRepository.cs
+++ src/Adapters/MongoDBDataAccess/MongoBaseRepository.cs
@@ -1,4 +1,5 @@
-﻿using MongoDB.Driver;
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
 
 namespace MongoDBDataAccess;
 
@@ -15,13 +16,15 @@ public class MongoBaseRepository
     // Salva uma lista de itens em uma coleção
     public async Task SaveItemsAsync<T>(string collectionName, IEnumerable<T> items)
     {
+        // Cria a coleção se não existir
+        await VerifyCollectionExistsAsync<T>(collectionName);
+
         var collection = _database.GetCollection<T>(collectionName);
         await collection.InsertManyAsync(items);
     }
 
     // Busca uma lista paginada de itens de uma coleção
-    public async Task<List<T>> GetItemsPaginatedAsync<T>(
-        string collectionName, int pageNumber, int pageSize)
+    public async Task<List<T>> GetItemsPaginatedAsync<T>(string collectionName, int pageNumber, int pageSize)
     {
         var collection = _database.GetCollection<T>(collectionName);
         return await collection
@@ -29,5 +32,19 @@ public class MongoBaseRepository
             .Skip((pageNumber - 1) * pageSize)
             .Limit(pageSize)
             .ToListAsync();
+    }
+
+    private async Task VerifyCollectionExistsAsync<T>(string collectionName)
+    {
+        var filter = new BsonDocument("name", collectionName);
+        var options = new ListCollectionsOptions { Filter = filter };
+
+        using (var cursor = await _database.ListCollectionsAsync(options))
+        {
+            if (!await cursor.AnyAsync())
+            {
+                await _database.CreateCollectionAsync(collectionName);
+            }
+        }
     }
 }
---
+++ src/Core/ePortfolio.Domain/Ports/MongoDB/IMediaService.cs
@@ -0,0 +1,26 @@
+﻿namespace ePortfolio.Domain.Ports.MongoDB;
+
+public interface IMediaService
+{
+    /// <summary>
+    /// Salva uma lista de mídia.
+    /// </summary>
+    Task<bool> SaveMediaItemsAsync(IEnumerable<MediaItem> mediaItems);
+
+    /// <summary>
+    /// Retorna uma lista paginada de mídia.
+    /// </summary>
+    Task<List<MediaItem>> GetMediaItemsPaginatedAsync(int pageNumber, int pageSize);
+}
+
+public class MediaItem
+{
+    public string Id { get; set; }
+    public string Name { get; set; }
+    public string? Author { get; set; }
+    public long? Duration { get; set; }
+    public string Url { get; set; }
+    public string Type { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.Now;
+    public long Size { get; set; }
+}

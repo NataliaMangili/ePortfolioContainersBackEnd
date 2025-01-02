9dcd88ec76f9273f78c70335f43ed64e8ded0df9
Natalia.Mangili <nataliaperico89@gmail.com>
2025-01-02 10:16:08 -0300

    feat: Interface e Referencia MongoDB Midia

---
+++ src/Core/ePortfolio.Domain/Ports/MongoDB/IMidiaRepository.cs
@@ -0,0 +1,19 @@
+﻿using MongoDBDataAccess.Services;
+
+namespace ePortfolio.Domain.Ports.MongoDB;
+
+public interface IMediaService
+{
+    /// <summary>
+    /// Salva uma lista de mídia.
+    /// </summary>
+    /// <param name="mediaItems">Lista de itens de mídia a serem salvos.</param>
+    Task SaveMediaItemsAsync(IEnumerable<MediaItem> mediaItems);
+
+    /// <summary>
+    /// Retorna uma lista paginada de mídia.
+    /// </summary>
+    /// <param name="pageNumber">Número da página</param>
+    /// <param name="pageSize">Quantidade de itens por página.</param>
+    Task<List<MediaItem>> GetMediaItemsPaginatedAsync(int pageNumber, int pageSize);
+}

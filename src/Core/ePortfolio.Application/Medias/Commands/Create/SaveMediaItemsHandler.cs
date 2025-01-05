b06420c3f1c3a335b9060ab59d60e19818a9c65f
Natalia.Mangili <nataliaperico89@gmail.com>
2025-01-05 10:22:01 -0300

    wip: Adicionando salvar no Cache Redis em SaveMediaItemsHandler

--- src/Core/ePortfolio.Application/Medias/Commands/Create/SaveMediaItemsHandler.cs
+++ src/Core/ePortfolio.Application/Medias/Commands/Create/SaveMediaItemsHandler.cs
@@ -1,15 +1,18 @@
-﻿using ePortfolio.Domain.Ports.MinIO;
+﻿using ePortfolio.Domain.Ports;
+using ePortfolio.Domain.Ports.MinIO;
 using ePortfolio.Domain.Ports.MongoDB;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.Extensions.Logging;
 
 namespace ePortfolio.Application.Medias.Commands.Create;
 
-public class SaveMediaItemsHandler(ILogger<SaveMediaItemsCommand> logger, IFileService minio, IMediaService mongo) : IRequestHandler<SaveMediaItemsCommand, bool>
+public class SaveMediaItemsHandler(ILogger<SaveMediaItemsCommand> logger, IFileService minio, IMediaService mongo, ICacheService redis) : IRequestHandler<SaveMediaItemsCommand, bool>
 {
     private readonly ILogger<SaveMediaItemsCommand> _logger = logger;
     private readonly IFileService _minioStorage = minio;
     private readonly IMediaService _mongoService = mongo;
+    private readonly ICacheService _redisCache = redis;
 
     public async Task<bool> Handle(SaveMediaItemsCommand command, CancellationToken cancellationToken)
     {
@@ -18,12 +21,22 @@ public class SaveMediaItemsHandler(ILogger<SaveMediaItemsCommand> logger, IFileS
         IEnumerable<Task<MinioFileReturn>> uploadTasks = command.DTO.File.Select(file => UploadFileAsync(file));
         MinioFileReturn[] uploadedMedia = await Task.WhenAll(uploadTasks);
 
+        await UpdateCache();
+
         // Valida se todos os uploads retornaram sucesso
         if (uploadedMedia.Any(media => !media.Success)) { throw new InvalidOperationException("One or more files failed to upload to MinIO."); }
 
         await CreateAndInsertInMongo(uploadedMedia);
 
         return true;
+    }
+
+    private async Task UpdateCache()
+    {
+        // Atualizar o cache com as 3 primeiras imagens
+        List<MediaItem> topImages = (await _mongoService.GetMediaItemsPaginatedAsync(1, 3, item => item.Type == EType.image));
+
+        await _redisCache.SetAsync("Top3Images", topImages);
     }
 
     private async Task<MinioFileReturn> UploadFileAsync(IFormFile file)
